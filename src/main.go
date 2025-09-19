f8d686dba79410ad60b1e99701989cd55c513d70
Yago <yagodev10@gmail.com>
2025-09-19 15:20:34 -0300

    GoLang
    Variáveis, Valores & Tipos

--- src/main.go
+++ src/main.go
@@ -3,35 +3,46 @@ package main
 // := declaração curta de variável so pode ser usada dentro de funções/code blocks
 // var declaração normal de variável pode ser usada em qualquer lugar do código
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
-	"os"
-	"os/exec"
-	"strings"
+	//"os"
+	//"os/exec"
+	//"strings"
 )
 
+var y int = 10
+
 func main() {
-	fmt.Print("Deseja abrir o Bloco de Notas? (s/n): ")
-	reader := bufio.NewReader(os.Stdin)
-	resposta, _ := reader.ReadString('\n')
-	resposta = strings.TrimSpace(resposta)
+	z := 20
+	qualquercoisa(z)
 
-	if resposta == "s" || resposta == "S" {
-		fmt.Println("Abrindo o Bloco de Notas...")
-		cmd := exec.Command("notepad.exe")
-		cmd.Start()
-	} else {
-		fmt.Println("Operação cancelada.")
-	}
-	//x := 10
-	//y := "apenas"
-	//fmt.Printf("x: %v, y: %v", x, x)
-	//fmt.Printf("y: %v, x: %v", y, y)
-	// %v : para valor
-	// %T : para tipo da variável
-	// %d : para números decimais
-	// %b : para números binários
-	// %#u : para unicode
-	// %#x : para números hexadecimal
-
+	// marmota sempre dentro dos metodos.
+	// var e fora. packet level scope
 }
+func qualquercoisa(x int) {
+	fmt.Println(y)
+	fmt.Println(x)
+}
+
+// fmt.Print("Deseja abrir o Bloco de Notas? (s/n): ")
+// reader := bufio.NewReader(os.Stdin)
+// resposta, _ := reader.ReadString('\n')
+// resposta = strings.TrimSpace(resposta)
+
+// if resposta == "s" || resposta == "S" {
+//fmt.Println("Abrindo o Bloco de Notas...")
+//cmd := exec.Command("notepad.exe")
+//cmd.Start()
+//} else {
+//fmt.Println("Operação cancelada.")
+//}
+//x := 10
+//y := "apenas"
+//fmt.Printf("x: %v, y: %v", x, x)
+//fmt.Printf("y: %v, x: %v", y, y)
+// %v : para valor
+// %T : para tipo da variável
+// %d : para números decimais
+// %b : para números binários
+// %#u : para unicode
+// %#x : para números hexadecimal
